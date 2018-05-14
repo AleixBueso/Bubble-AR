2b2a8cacc81269a639cc9be02d5651589570b777
Señora Catolica <miquel_giru_96@hotmail.com>
2018-05-14 21:11:09 +0200

    bubble and shooter

---
+++ Assets/Bubble.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+
+    public Color color;
+    public float radius;
+    [SerializeField] Renderer render;
+    [SerializeField] Collider col;
+
+
+    private void Start()
+    {
+        color = render.material.color;
+    }
+
+    public Collider[] GetBubbleColliders()
+    {
+        Collider[] ret = Physics.OverlapSphere(transform.position, radius);
+        return ret;
+    }
+
+}
---
+++ Assets/Shooter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+
+    [SerializeField] Transform bubble;
+    private Transform spawnPosition;
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+        if(Input.GetButton("Fire1"))
+        {
+            CreateBubble();
+        }
+	}
+
+    private void CreateBubble()
+    {
+        GameObject go = Instantiate(bubble, spawnPosition.position, transform.rotation).gameObject;
+        go.GetComponent<Rigidbody>().AddForce(transform.forward, ForceMode.Impulse);
+    }
+}
